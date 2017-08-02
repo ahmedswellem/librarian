e0f892ce1de5b13cb6dc4f37fb1b898fdac86670
ahmedswellem <30528550+ahmedswellem@users.noreply.github.com>
2017-08-03 02:46:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const tutorials = [
     title: "Backyard",
     description: "Travis Scott - Days Before the Rodeo",
     iframe: "<iframe width='100%' height='600' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163746355&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>",
-    thumbnail: "https://i1.sndcdn.com/artworks-000088468281-q999dc-t200x200.jpg"
+    thumbnail: "https://myblabber.com/be-master/uploads/category_image/1--.jpg"
   },
   {
     id: 2,
